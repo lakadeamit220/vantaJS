24eac9ae255c01a40fad70087523403a00f450ff
Amit Lakade <99075733+lakadeamit220@users.noreply.github.com>
2025-05-29 10:38:40 +0530

    changes the color of nodes and background of VantaNet

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,10 @@ function App() {
   return (
     <>
       {/* <VantaGlobe/> */}
-      <VantaNet>
+      <VantaNet
+        color="#ff3366" // Pink nodes
+        backgroundColor="#001122" // Dark blue background
+      >
         <h1 className="text-5xl font-bold text-red-600 text-center py-5">
           VantaJS
         </h1>
